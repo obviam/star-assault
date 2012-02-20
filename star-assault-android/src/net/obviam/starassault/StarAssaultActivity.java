3ecdc83cc68a8ee30b5f980ee4923e1b5ef7d241
Impaler <impaler@obviam.net>
2012-02-20 15:02:31 +0000

    Initial commit

---
+++ star-assault-android/src/net/obviam/starassault/StarAssaultActivity.java
@@ -0,0 +1,13 @@
+package net.obviam.starassault;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class StarAssaultActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
