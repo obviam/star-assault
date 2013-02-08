555867b1fb61434059031025fe79fb7983560089
Impaler <impaler@obviam.net>
2013-02-08 15:50:08 +0000

    Added texture atlases to assets

---
+++ star-assault/src/net/obviam/starassault/utils/TextureSetup.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package net.obviam.starassault.utils;
+
+//import com.badlogic.gdx.tools.imagepacker.TexturePacker2;
+
+/**
+ * @author tamas
+ *
+ */
+public class TextureSetup {
+
+	public static void main(String[] args) {
+//		TexturePacker2.process("/Users/tamas/tmp/star-guard-assets/", "/Users/tamas/tmp/sga/", "textures.pack");
+	}
+}
